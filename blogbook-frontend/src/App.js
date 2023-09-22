350d78de1cea9d2a0bf329082ed21950dbefe59f
Jonathan Voris <jon@voris.nyc>
2023-09-22 22:27:58 +0700

    Setting up routes using react-router-dom

--- blogbook-frontend/src/App.js
+++ blogbook-frontend/src/App.js
@@ -1,14 +1,67 @@
-import logo from './logo.svg';
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Route,
+  Outlet
+} from "react-router-dom";
+
+import Activity from "./pages/Activity"
+import Login from "./pages/Login"
+import Post from "./pages/Post"
+import Register from "./pages/Register"
+import View from "./pages/View"
+
 import './App.css';
+
+const Layout = ()=>{
+  return (
+    <>
+        <div id="app" style={({ height: "100vh" }, { display: "flex" })}>
+          <Outlet></Outlet>
+        </div>
+    </>
+  )
+}
+
+const router = createBrowserRouter(
+  [
+    {
+      path: "/",
+      element: <Layout></Layout>,
+      children:[
+        {
+          path: "/",
+          element: <Activity></Activity>
+        },
+        {
+          path: "/activity",
+          element: <Activity></Activity>
+        },
+        {
+          path: "/Login",
+          element: <Login></Login>
+        },
+        {
+          path: "/Post",
+          element: <Post></Post>
+        },
+        {
+          path: "/register",
+          element: <Register></Register>
+        },
+        {
+          path: "/View/:post_id",
+          element: <View></View>
+        }
+      ]
+    }
+  ]
+)
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <p>
-          Hello World!
-        </p>
-      </header>
+      <RouterProvider router={router}/>
     </div>
   );
 }
---
+++ blogbook-frontend/src/pages/Register.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const Register = () => {
+    return (
+        <div>Register</div>
+    );
+}
+export default Register;
